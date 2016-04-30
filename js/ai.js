e790e81319d54e40189b19fb8cec1a5f40e5d879
Liang-Chieh Chen <luniacslime@gmail.com>
2016-04-30 18:31:57 +0900

    Fix bug: dont use obs[0] as first obstacle
    Should check if obstacle is in front of tRex

--- js/ai.js
+++ js/ai.js
@@ -10,15 +10,19 @@ ai.init = function(){
 ai._mainLoop = function(){
   obs = api.getObstacles();
   if (obs.length != 0){
-    var bound = 280 * (api.getCurrentSpeed() / Runner.config.MAX_SPEED);
+    var bound = 350 * (api.getCurrentSpeed() / Runner.config.MAX_SPEED);
     var distance = (obs[0].xPos + obs[0].width) - api.getPlayer().xPos;
+    var next = 0;
+    if (distance < 0)
+      next = 1;
+    distance = (obs[next].xPos + obs[next].width) - api.getPlayer().xPos;
     var player = api.getPlayer();
-    if (0 < distance && distance < bound && obs[0].yPos != 50 && obs[0].yPos != 75 && player.status != 'JUMPING'){
+    if (0 < distance && distance < bound && obs[next].yPos != 50 && obs[next].yPos != 75 && player.status != 'JUMPING'){
       api.duckStop();
       api.jump();
     }
     else if (player.status == 'JUMPING'){
-      if (player.xPos > obs[0].xPos + obs[0].width && !this.speedDroping){
+      if (player.xPos > obs[0].xPos + obs[0].width / 2 && !this.speedDroping){
         api.speedDrop();
         this.speedDroping = true;
       }
